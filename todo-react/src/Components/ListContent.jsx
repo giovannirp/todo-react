6126453d0feadaadbf46f678c56beef714655e6c
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-12-01 21:05:20 -0300

    fix: adicionando novas funcionalidades no projeto e alterando css

--- todo-react/src/Components/ListContent.jsx
+++ todo-react/src/Components/ListContent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Coments } from "./Coments";
 import { v4 as uuidv4 } from 'uuid';
+import { PlusCircle } from "phosphor-react";
+import Clipboard from '../assets/clipboard.png';
 
 import styles from "./ListContent.module.css";
 
@@ -8,17 +10,17 @@ import { notes } from "./notes.jsx";
 
 export function ListContent() {
   const [newTasksComents, setNewTasksComents] = useState([
-    "Javascript"
+    "javascript"
   ]);
   const [newTasksInput, setNewTasksInput] = useState("");
 
   const handleNewTasks = (event) => {
+    event.target.setCustomValidity("");
     setNewTasksInput(event.target.value);
   };
 
   const handleCreateTasks = (event) => {
     event.preventDefault();
-    console.log("aquii");
     setNewTasksComents([...newTasksComents, newTasksInput]);
     setNewTasksInput("");
   };
@@ -31,28 +33,46 @@ export function ListContent() {
     setNewTasksComents(commentsDeleteOne);
   };
 
+  const handleNewTaskvalid = (event) => {
+    event.target.setCustomValidity("Esse campo é obrigatório...")
+  }
+  
+  const isTasksComments = newTasksInput.length === 0;
+
   return (
     <section className={styles.listContent}>
       <div>
-        <form onSubmit={handleCreateTasks}>
+        <form onSubmit={handleCreateTasks} className={styles.addTasks}>
           <input
             type="text"
             value={newTasksInput}
             placeholder="Adicione uma nova tarefa"
             onChange={handleNewTasks}
+            onInvalid={handleNewTaskvalid}
+            required
           />
-          <button type="submit">Criar</button>
+          <button type="submit" disabled={isTasksComments}>
+            <span>Criar</span> <PlusCircle size={17} />
+          </button>
         </form>
       </div>
       <p>Tarefas criadas {newTasksComents.length}</p>
       {newTasksComents.map((comment) => {
-        console.log(uuidv4())
         return (
           <div>
             <Coments key={uuidv4()} content={comment} onDelete={deleteComent} />
           </div>
         );
       })}
+      {newTasksComents.length === 0 && (
+        <div>
+          <img src={Clipboard} title="Imagem Clipboard" />
+          <p>
+            Você ainda não tem tarefas cadastradas <br />
+            Crie tarefas e organize seus itens a fazer 
+          </p>
+        </div>
+      )}
     </section>
   );
 }
